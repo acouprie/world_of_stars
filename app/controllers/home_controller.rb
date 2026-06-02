0da9e2b723852c5314435f9bdaad90a32275dfb1
acouprie <antoine@couprie.dev>
2026-06-02 15:34:12 +0200

    redirect to planet if logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,10 @@
 class HomeController < ApplicationController
   allow_unauthenticated_access
+
+  def index
+    if authenticated?
+      planet = Current.user.planets.first
+      redirect_to planet_path(planet) if planet
+    end
+  end
 end
--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,9 +2,33 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
   describe "GET /" do
-    it "is accessible without authentication" do
-      get "/"
-      expect(response).to have_http_status(:success)
+    context "when unauthenticated" do
+      it "is accessible and returns success" do
+        get "/"
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "when authenticated" do
+      let(:user) { create(:user) }
+
+      before { sign_in(user) }
+
+      context "with a planet" do
+        let!(:planet) { create(:planet, :player, user: user) }
+
+        it "redirects to the planet" do
+          get "/"
+          expect(response).to redirect_to(planet_path(planet))
+        end
+      end
+
+      context "without a planet" do
+        it "stays on the root page" do
+          get "/"
+          expect(response).to have_http_status(:success)
+        end
+      end
     end
   end
 end
